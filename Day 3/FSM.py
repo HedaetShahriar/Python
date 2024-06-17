eea00705b935e1ef398ca00acbb2bef749efcf21
MD. HEDAET SHAHRIAR HIMON <shahriahedaet@gmail.com>
2024-06-17 22:02:43 +0600

    1

--- Day 3/FSM.py
+++ Day 3/FSM.py
@@ -1,4 +1,18 @@
 state=1
 while(1):
     transit=input("Enter a letter: ")
-    
+    print("You have entered ",transit)
+    if state==1:
+        if transit=='A':
+            state=2
+    elif state==2:
+        if transit=='B':
+            state=3
+        elif transit=='C':
+            state=1
+    elif state==3:
+        if transit=='D':
+            state=1
+    print("Right now you are at state: ",state)
+    if transit=='E':
+        break
