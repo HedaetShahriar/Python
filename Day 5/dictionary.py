77de640b1f7c7eba8a38de57152c77b4d448b855
MD. HEDAET SHAHRIAR HIMON <shahriahedaet@gmail.com>
2024-06-21 22:29:42 +0600

    dictionary

---
+++ Day 5/dictionary.py
@@ -0,0 +1,13 @@
+dictionary={"Name":["H","I","M","O","N"],
+            "Age":[27,12,11,52,25]}
+print(dictionary["Name"][0])
+
+db={}
+
+db["name"]=["Himon","Bipul"]
+db["age"]=[24,25]
+print(db)
+#update
+db["name"].append('j')
+db["age"].append(1)
+print(db)
